2e1d5612ba8b350a2972695a1e58f36f6a0bc68f
michael mentele <michaelrmentele@gmail.com>
2021-12-12 18:34:25 -0600

    add basic setup commands for cli tools

---
+++ cli_tools_setup.sh
@@ -0,0 +1,7 @@
+# setup oh-my-zsh https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# setup oh-my-vim: https://github.com/liangxianzhe/oh-my-vim
+curl -L https://raw.github.com/liangxianzhe/oh-my-vim/master/tools/prepare_mac.sh | sh
+curl -L https://raw.github.com/liangxianzhe/oh-my-vim/master/tools/install.sh | sh
+
